bbd3d3be3c36834a0ca1e9480b08cc22a9f8c5b2
Marcos Elena San Segundo <marcos.elesan@educa.jcyl.es>
2022-11-03 17:10:41 +0100

    Creacion de la clase libros

--- src/Libros.java
+++ src/Libros.java
@@ -1,2 +1,43 @@
-public class Libros {
+public class libros {
+    private String titulo;
+    private String isbn;
+    private String autor;
+    private String genero;
+    private String fecha_publicacion;
+
+    public String getTitulo(){
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getIsbn(){
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    public String getAutor(){
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public String getGenero(){
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    public String getFecha_publicacion(){
+        return fecha_publicacion;
+    }
+
+    public void setFecha_publicacion(String fecha_publicacion) {
+        this.fecha_publicacion = fecha_publicacion;
+    }
 }
